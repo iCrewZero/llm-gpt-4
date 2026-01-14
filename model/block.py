e4d359afa7a3814cd3655e1c2f7b97df9bbc7aaf
Dontamsetty-Tribhuvan <tribhuvan.studyforlife@gmail.com>
2026-01-14 17:47:45 +0530

    Refactor Block class with new initialization and forward method

---
+++ model/block.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+from .attention import CausalSelfAttention
+from .rmsnorm import RMSNorm
+
+class Block(nn.Module):
+    def __init__(self, dim, heads, mlp_ratio=4):
+        super().__init__()
+        self.norm1 = RMSNorm(dim)
+        self.attn = CausalSelfAttention(dim, heads)
+        self.norm2 = RMSNorm(dim)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, dim * mlp_ratio),
+            nn.GELU(),
+            nn.Linear(dim * mlp_ratio, dim)
+        )
+
+    def forward(self, x, rope, start_pos=0):
+        x = x + self.attn(self.norm1(x), rope, start_pos)
+        x = x + self.mlp(self.norm2(x))
+        return x

a0fa8eaa630ec72eff08e5011145854610c61e0e
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-16 08:48:49 +0530

    Update gpt.py

--- model/gpt.py
+++ model/gpt.py
@@ -1,16 +1,27 @@
 import torch
 import torch.nn as nn
-from model.blocks import Block
+from .block import Block
 
 class GPT(nn.Module):
-    def __init__(self, vocab_size, d_model, n_layers, n_heads):
+    def __init__(self, vocab_size, dim, layers, heads, max_len=2048):
         super().__init__()
-        self.embed = nn.Embedding(vocab_size, d_model)
-        self.blocks = nn.ModuleList([Block(d_model, n_heads) for _ in range(n_layers)])
-        self.lm_head = nn.Linear(d_model, vocab_size, bias=False)
+        self.embed = nn.Embedding(vocab_size, dim)
+        self.pos = nn.Embedding(max_len, dim)
 
-    def forward(self, token_ids, state, allocator):
-        x = self.embed(token_ids)
-        for block in self.blocks:
-            x = block(x, state, allocator)
-        return self.lm_head(x)
+        self.blocks = nn.ModuleList([
+            Block(dim, heads) for _ in range(layers)
+        ])
+
+        self.ln = nn.LayerNorm(dim)
+        self.head = nn.Linear(dim, vocab_size, bias=False)
+
+    def forward(self, input_ids):
+        B, T = input_ids.shape
+        pos = torch.arange(T, device=input_ids.device)
+
+        x = self.embed(input_ids) + self.pos(pos)
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.ln(x)
+        return self.head(x)

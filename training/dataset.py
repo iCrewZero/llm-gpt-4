685b54cea63bc2b67d17c6a1aa2598dc0eb04055
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 21:20:49 +0530

    Update dataset.py

--- training/dataset.py
+++ training/dataset.py
@@ -1,19 +1,16 @@
-import numpy as np
 import torch
 
-class BinDataset(torch.utils.data.Dataset):
-    def __init__(self, path, block_size):
-        self.data = np.memmap(path, dtype=np.uint16, mode="r")
-        self.block_size = block_size
+class TokenDataset(torch.utils.data.Dataset):
+    def __init__(self, tokens, seq_len):
+        self.tokens = tokens
+        self.seq_len = seq_len
 
     def __len__(self):
-        return len(self.data) - self.block_size
+        return len(self.tokens) - self.seq_len
 
     def __getitem__(self, idx):
-        x = torch.from_numpy(
-            self.data[idx : idx + self.block_size].astype(np.int64)
-        )
-        y = torch.from_numpy(
-            self.data[idx + 1 : idx + self.block_size + 1].astype(np.int64)
-        )
-        return x, y
+        chunk = self.tokens[idx:idx+self.seq_len+1]
+        return {
+            "input_ids": torch.tensor(chunk[:-1]),
+            "labels": torch.tensor(chunk)
+        }

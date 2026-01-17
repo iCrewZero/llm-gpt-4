536fe174acd4c0cc0f8b50d13fe8b89dd9add9ea
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 21:22:11 +0530

    Update checkpoint.py

--- training/checkpoint.py
+++ training/checkpoint.py
@@ -1,15 +1,14 @@
 import torch
-import os
 
-def save(model, opt, step, path="ckpt.pt"):
+def save_checkpoint(model, opt, step, path):
     torch.save({
         "model": model.state_dict(),
         "opt": opt.state_dict(),
         "step": step
     }, path)
 
-def load(model, opt, path="ckpt.pt"):
-    ckpt = torch.load(path)
+def load_checkpoint(model, opt, path):
+    ckpt = torch.load(path, map_location="cpu")
     model.load_state_dict(ckpt["model"])
     opt.load_state_dict(ckpt["opt"])
     return ckpt["step"]

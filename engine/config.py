d70760d6f20008524dc738a72799c6a6b7c2f4fd
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 10:26:44 +0530

    Create config.py

---
+++ engine/config.py
@@ -0,0 +1,12 @@
+class EngineConfig:
+    page_size = 16
+    max_pages = 4096
+
+    speculative_k = 4
+    max_batch = 16
+
+    mcts_simulations = 32
+    mcts_depth = 8
+
+    enable_prm = True
+    enable_mcts = True

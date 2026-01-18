4cd1ce80973d9ef27f5fe04bba4bf8374685d0a2
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-18 10:10:25 +0530

    Update mla.py

--- model/mla.py
+++ model/mla.py
@@ -1,15 +1,12 @@
 import torch
 import torch.nn as nn
 
-class MLA(nn.Module):
+class MultiHeadLatentAttention(nn.Module):
     def __init__(self, dim, n_head):
         super().__init__()
-        self.latent = nn.Parameter(torch.randn(n_head, dim//n_head))
-        self.proj = nn.Linear(dim, dim, bias=False)
+        self.n_head = n_head
+        self.latent = nn.Linear(dim, dim, bias=False)
 
     def forward(self, x):
-        B,T,C = x.shape
-        h = x.view(B,T,-1,self.latent.size(-1))
-        scores = (h * self.latent).sum(-1, keepdim=True)
-        h = h * scores
-        return self.proj(h.reshape(B,T,C))
+        latent = self.latent(x)
+        return x + latent
